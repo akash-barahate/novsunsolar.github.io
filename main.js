86ed5d824ec5ada45bd6a335c1dda947caa57f48
Akash Barahate <akashbarahate07@gmail.com>
2024-09-10 08:36:01 +0530

    Form Design Complete Manipulation yet not done

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let formPrint = document.getElementById("formPrint");
-
+let printSection = document.getElementById("printSection");
+printSection.style.display = "none";
 function printContent() {
+  printSection.style.display = "block";
   window.print();
 }
 
